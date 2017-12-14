122fcc1ba0bff99373626738af191e80a543f3ec
Georg Sandholm <georg.sandholm@xlent.se>
2017-12-14 16:16:02 +0100

    - Newline cleanup

--- src/Core/CustomRedirects/CustomRedirectCollection.cs
+++ src/Core/CustomRedirects/CustomRedirectCollection.cs
@@ -66,7 +66,7 @@ namespace BVNetwork.NotFound.Core.CustomRedirects
             // Common case
             if (foundRedirect == null)
             {
-                url = urlNotFound.PathAndQuery; ;
+                url = urlNotFound.PathAndQuery;
                 foundRedirect = FindInternal(url);
             }
 
